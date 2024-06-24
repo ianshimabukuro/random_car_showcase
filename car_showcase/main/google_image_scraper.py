983e1038af1b77fdee0b8a615427e18e4aaccbda
ianshimabukuro <shimakun000@gmail.com>
2024-06-24 19:58:09 +0900

    Added a css file. Made the google CSE work and the api is also working.

--- car_showcase/main/google_image_scraper.py
+++ car_showcase/main/google_image_scraper.py
@@ -7,9 +7,9 @@ import re
 
 def get_image_url(query):
     formats = ['jpeg','png','gif','img']
-    API = 'AIzaSyCLr1MvhuAUFcc3Hj34G2QtsRQFVJ6pmYg'
+    API = 'AIzaSyDPqCUReJ4p83bxxxQBvRZitNA_vbHL9Bs'
     searchEngineID = 'c6fb1df3b10dd4278'
-    url = 'https://www.googleapis.com/customsearch/v1?key=' + API + '&cx=' + searchEngineID + '&q=' + query +'&searchType=image'
+    url = 'https://www.googleapis.com/customsearch/v1?key=' + API + '&cx=' + searchEngineID + '&q=' + query +'&searchType=image&fileType=jpeg'
     result = []
     response = requests.get(url)
     urls = Find(response.text)
@@ -17,8 +17,7 @@ def get_image_url(query):
         for format in formats:
             if format in url:
                 result.append(url)
-
-    return response.text
+    return result
 
 def Find(string):
     # findall() has been used
@@ -27,6 +26,6 @@ def Find(string):
     url = re.findall(regex, string)
     return [x[0] for x in url]
 
-src = get_image_url("audi quattro")
+src = get_image_url("audi a4 1996")
 dict_image = src
 print(src)
--- car_showcase/main/views.py
+++ car_showcase/main/views.py
@@ -10,20 +10,15 @@ from . import google_image_scraper
 def showCars(request):
 
     page = random.randint(0,49)
-    make = "audi"
+    make = "toyota"
     api_url = 'https://api.api-ninjas.com/v1/cars?limit=50&make={}'.format(make)
     response = requests.get(api_url, headers={'X-Api-Key': 'jFQIehzE63CSUj5jaggNLg==AiW7XrkI3zZvIsI6'})
     data = response.json()[page]
 
 
-    query = data['model']
+    query = data['make']+data['model']+str(data['year'])
     src = google_image_scraper.get_image_url(query)
-    dict_image = src
-    print(src)
+    dict_image = {'src': src}
     data.update(dict_image)
-
-
-
-
     return render(request,'main/home.html',data)
 
